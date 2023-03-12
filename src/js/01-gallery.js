f6fdeed781e14f7c5d528578c7a89ae9305d7508
Iryna B <imbigdash@gmail.com>
2023-03-12 16:12:07 +0000

    added 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryImageCard = makeGalleryItems(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImageCard);
 
-console.log(galleryItems);
+function makeGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <a class="gallery__link" href='${original}'>
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  `;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
