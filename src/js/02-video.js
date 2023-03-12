39271abbf02cb6b85abcf8a30a0b583930707d4c
Iryna B <imbigdash@gmail.com>
2023-03-12 22:28:08 +0000

    added 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
+currentTimeFn();
+
+function currentTimeFn() {
+  const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  if (array) {
+    const currentTime = array['seconds'];
+    player.setCurrentTime(currentTime);
+  }
+}
